fe1810304393e4f169a34a0abf125b7c9a443d16
Vadim Korotkov <vadik.kortkov@yandex.ru>
2021-04-23 23:35:40 +0300

    feat: 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,32 @@
-let OpenEditForm = document.querySelector('.edit-button');
+let openEditForm = document.querySelector('.profile__edit-button');
 let popup = document.querySelector ('.popup');
-let CloseEditForm = document.querySelector ('.close-button');
-let formElement = document.querySelector ('.edit-form');
+let formElement = document.querySelector ('.popup__form');
+let closeEditForm = document.querySelector ('.popup__form-close');
 let nameInput = formElement.querySelector ('#name');
 let jobInput = formElement.querySelector ('#job');
-let save = formElement.querySelector ('.edit-form__save');
 let profileTitle = document.querySelector ('.profile__info-title');
 let profileSubtitle = document.querySelector ('.profile__info-subtitle');
-
+nameInput.value= profileTitle.textContent;
+jobInput.value = profileSubtitle.textContent;
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = `${nameInput.value}`;
     profileSubtitle.textContent = `${jobInput.value}`;
-    popup.classList.toggle ('popup__open');
-   }
-
-function ToglePopup (event) {
-    event.preventDefault ();
-    popup.classList.toggle ('popup__open');
-    nameInput.value= profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
+    toglePopup(popup);
 }
 
+function togleMainPopup (event) {
+    event.preventDefault ();
+    toglePopup (popup);
+}
 
-OpenEditForm.addEventListener ('click', ToglePopup);
-CloseEditForm.addEventListener ('click', ToglePopup);
+function toglePopup (target) {
+    target.classList.toggle ('popup_open');
+}
+
+openEditForm.addEventListener ('click', togleMainPopup);
+closeEditForm.addEventListener ('click', togleMainPopup);
 formElement.addEventListener('submit', formSubmitHandler); 
 
 
